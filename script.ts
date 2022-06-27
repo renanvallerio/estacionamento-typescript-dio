8e362168759c4b5a0c140f4060ca9deb61b2433f
renanvallerio <renanvallerio@gmail.com>
2022-06-26 16:58:46 -1000

    Aula 4

--- script.ts
+++ script.ts
@@ -8,9 +8,15 @@ interface Veiculo {
   const $ = (query: string): HTMLInputElement | null => document.querySelector(query);
 
   function patio(){
-    function ler(){}
+    function ler(): Veiculo[]{
+      return localStorage.patio ? JSON.parse(localStorage.patio) : [];
+    }
 
-    function adicionar(veiculo: Veiculo){
+    function salvar(veiculos: Veiculo[]){
+      localStorage.setItem("patio", JSON.stringify(veiculos));
+    }
+
+    function adicionar(veiculo: Veiculo, salva?: boolean){
       const row = document.createElement("tr");
 
       row.innerHTML = `
@@ -23,16 +29,25 @@ interface Veiculo {
       `;
 
       $("#patio")?.appendChild(row);
+
+      if (salva) salvar([...ler(), veiculo]);
     }
 
     function remover(){}
 
-    function salvar(){}
-
-    function render(){}
+    function render(){
+      $("#patio")!.innerHTML = "";
+      const patio = ler();
+      
+      if (patio.length){
+        patio.forEach((veiculo) => adicionar(veiculo));
+      }
+    }
 
     return {ler, adicionar, remover, salvar, render}
   }
+
+  patio().render();
 
   $("#cadastrar")?.addEventListener('click', () => {
     const nome = $("#nome")?.value;
@@ -43,6 +58,6 @@ interface Veiculo {
       return;
     }
     
-    patio().adicionar({nome, placa, entrada: new Date()})
+    patio().adicionar({nome, placa, entrada: new Date()}, true);
   });
 })();
