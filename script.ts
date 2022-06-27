6a35408eb1cc83fa0239908433be8612c076d1cb
renanvallerio <renanvallerio@gmail.com>
2022-06-26 17:43:35 -1000

    Aula 5

--- script.ts
+++ script.ts
@@ -1,11 +1,19 @@
 interface Veiculo {
   nome: string;
   placa: string;
-  entrada: Date;
+  entrada: Date | string;
+  clientId?: string;
 }
-
+  
 (function() {
   const $ = (query: string): HTMLInputElement | null => document.querySelector(query);
+
+  function calcTempo(mil: number){
+    const min = Math.floor(mil/ 60000);
+    const sec = Math.floor(mil % 60000) / 1000;
+
+    return `${min}m e ${sec}s`;
+  }
 
   function patio(){
     function ler(): Veiculo[]{
@@ -28,12 +36,26 @@ interface Veiculo {
         </td>
       `;
 
+    row.querySelector(".delete")?.addEventListener("click", function(){
+      remover(this.dataset.placa)
+    })
+
       $("#patio")?.appendChild(row);
 
       if (salva) salvar([...ler(), veiculo]);
     }
 
-    function remover(){}
+    function remover(placa: string){
+
+      const {entrada, nome} = ler().find(veiculo => veiculo.placa === placa);
+
+      const tempo = calcTempo(new Date().getTime() - new Date(entrada).getTime());
+
+      if(!confirm(`O veiculo ${nome} permaneceu por ${tempo}min. Deseja encerrar?`)) return;
+      
+      salvar(ler().filter(veiculo => veiculo.placa !== placa));
+      render();
+    }
 
     function render(){
       $("#patio")!.innerHTML = "";
@@ -58,6 +80,6 @@ interface Veiculo {
       return;
     }
     
-    patio().adicionar({nome, placa, entrada: new Date()}, true);
+    patio().adicionar({nome, placa, entrada: new Date().toISOString()}, true);
   });
 })();
